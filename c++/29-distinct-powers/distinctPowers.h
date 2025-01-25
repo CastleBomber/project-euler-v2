ded57e9cc63e8d6c85bc4f8e773dab4acb2c7b69
cBombs <richie.ucsb@gmail.com>
2025-01-25 15:11:03 -0800

    gnu. mpz_class and get_mpz_t. mpz_pow_ui.

---
+++ c++/29-distinct-powers/distinctPowers.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <gmpxx.h>
+#include <unordered_set>
+
+using namespace std;
+
+int distinctPowers(int lowerLimit, int upperLimit);
+void removeDuplicates(vector<mpz_class>& terms);
+
+#endif
